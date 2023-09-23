74976c3767f955e8750b8f4071d270029f45850c
vipin-v <vipindas233@gmail.com>
2023-09-23 21:32:46 +0530

    practices

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-alert('Hello');
+document.querySelector("li a").style.color = "green"; 
+document.getElementById("bus").style.color = "orange";
+document.querySelector("button").style.backgroundColor = "Yellow";
+document.querySelector(".huge").classList.toggle("huge");
