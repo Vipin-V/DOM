85ddf4776ee28f24d19215231e6082502de6c75f
vipin-v <vipindas233@gmail.com>
2023-09-25 10:03:00 +0530

    drum challenge part 1 completed

---
+++ Drum Kit/index.js
@@ -0,0 +1,7 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    alert("i got clicked");
+  })
+}
