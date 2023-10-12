c73a05333e8434e61bdd50a766290015c660f00a
vipin-v <vipindas233@gmail.com>
2023-10-12 20:55:07 +0530

    added guess number game starter files

--- practice/index.js
+++ practice/index.js
@@ -1,31 +1,38 @@
 /* Write your code below. Good luck! 🙂 */
 
+'use strict';
+
 const mark = {
   fullName: 'Mark Miller',
   mass: 78,
   height: 1.64,
-  calcBMI: function(){
+  calcBMI: function () {
     this.bmi = this.mass / this.height ** 2;
     return this.bmi;
-  }
+  },
 };
 
 const john = {
   fullName: 'John Smith',
   mass: 92,
   height: 1.64,
-  calcBMI: function(){
+  calcBMI: function () {
     this.bmi = this.mass / this.height ** 2;
     return this.bmi;
-  }
+  },
 };
 
 mark.calcBMI();
 john.calcBMI();
 console.log(mark.bmi, john.bmi);
 
-if (john.bmi  > mark.bmi){
-  console.log(`${john.fullName}'s BMI (${john.bmi} is higher than ${mark.bmi})`);
-}else if (mark.bmi > john.bmi){
-  console.log(`${mark.fullName}'s BMI (${mark.bmi} is higher than ${john.bmi})`);
+if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi} is higher than ${mark.bmi})`
+  );
+} else if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi} is higher than ${john.bmi})`
+  );
 }
+console.log('helloworld');
