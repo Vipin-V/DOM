c056cf3ee8edfb679d9c206846ccdabefe4047cf
vipin-v <vipindas233@gmail.com>
2023-10-13 20:42:07 +0530

    guess game

--- Guess-My-Number/starter/script.js
+++ Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let highScore = 99;
+document.querySelector('.message').textContent = 'Correct Number!!';
+
+document.querySelector('.highscore').textContent = highScore;
+
+document.querySelector('.number').textContent = highScore;
+
+document.querySelector('.score').textContent = highScore;
+
+document.querySelector('.guess').value = highScore;
